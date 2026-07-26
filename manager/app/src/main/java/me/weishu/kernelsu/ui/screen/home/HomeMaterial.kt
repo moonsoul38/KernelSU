9d1352f2269efa556b7c0f2309ef63b61508842d
moonsoul38 <moonsoul388@outlook.com>
2026-07-26 12:38:41 +0800

    Remove GKI warning card from HomeMaterial
    Removed the GKI warning card from the home screen.

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMaterial.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMaterial.kt
@@ -96,9 +96,6 @@ fun HomePagerMaterial(
                     )
                 )
             }
-            if (state.showGkiWarning) {
-                WarningCard(stringResource(id = R.string.home_gki_warning))
-            }
             if (state.showUAPIMisMatchWarning) {
                 WarningCard(
                     stringResource(
